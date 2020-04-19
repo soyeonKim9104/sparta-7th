031495cd6ac8b8f0e084800fb8130deadb1a7eae
soyeonKim9104 <soyean7979@gmail.com>
2020-04-19 17:58:36 +0900

    2020-04-19

--- myproject/airline.py
+++ myproject/airline.py
@@ -1,25 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
 host = 'http://openapi.airport.co.kr/service'
 serviceKey = 'y7pMhOU8g9nUPMZeare2UibAJZm453CLckGI9xub5arSby6dFF6TjctFdkaC4PwVlzHN%2B1NRquo8o0%2Fdzo2L%2FA%3D%3D'
 
 #나라 명
 def get_air_code():
-    url = '{}/rest/AirportCodeList/getAirportCodeList?ServiceKey={}&pageNo={}'.format(host, serviceKey, 1)
-    headers = {
-        'Content-Type': 'application/xml'
-    }
-    res = requests.get(url, headers=headers)
+    for i in range(10):
+        url = '{}/rest/AirportCodeList/getAirportCodeList?ServiceKey={}&pageNo={}'.format(host, serviceKey, i + 1)
+        headers = {
+            'Content-Type': 'application/xml'
+        }
+        res = requests.get(url, headers=headers)
+        soup = BeautifulSoup(res.text, 'html.parser')
 
-    soup = BeautifulSoup(res.text, 'html.parser')
+        citycode = soup.select("item")
 
-    citycode = soup.select("item")
+        for city in citycode:
+            citynamekor = city.select_one('item > citykor').text
+            cityCode = city.select_one('item > cityCode').text
+            
+            if citynamekor is not None and cityCode is not None:
+                doc = {
+                    'citynamekor': citynamekor,
+                    'cityCode': cityCode
+                }
+                db.citycode.insert_one(doc)
 
-    for city in citycode:
-        citynamekor = city.select_one('item > citykor')
-        if citynamekor is not None:
-            print(citynamekor.text)
 
 #국내선 운항 스케줄 
 def get_air_d_flight():
@@ -34,8 +46,30 @@ def get_air_d_flight():
     flightdomestic = soup.select("item")
 
     for domestic in flightdomestic:
+        #출발공항
+        startcity = domestic.select_one('item > startcity').text
+        #출발시간
+        domesticStartTime = domestic.select_one('item > domesticStartTime').text
+        #도착공항
+        arrivalcity = domestic.select_one('item > arrivalcity').text
+        #도착시간
+        domesticArrivalTime = domestic.select_one('item > domesticArrivalTime').text
+        #항공사(국문)
+        airlineKorean = domestic.select_one('item > airlineKorean').text
+
+        print(startcity,domesticStartTime,arrivalcity,domesticArrivalTime,airlineKorean)
+
+        if startcity is not None and domesticStartTime is not None and arrivalcity is not None and domesticArrivalTime is not None and airlineKorean:
+            doc = {
+                'startcity' : startcity,
+                'domesticStartTime' : domesticStartTime,
+                'arrivalcity' : arrivalcity,
+                'domesticArrivalTime' : domesticArrivalTime,
+                'airlineKorean' : airlineKorean
+            }
+
+            db.flightdomestic.insert_one(doc)
     
-    print(res.text)
 
 def get_air_i_flight():
     url = '{}/rest/FlightScheduleList/getIflightScheduleList?ServiceKey={}&pageNo={}'.format(host, serviceKey, 1)
@@ -43,16 +77,18 @@ def get_air_i_flight():
         'Content-Type': 'application/xml'
     }
     res = requests.get(url, headers=headers)
-    print(res.text)
 
+    soup = BeautifulSoup(res.text, 'html.parser')
 
-def get_air_status_flight():
-    url = '{}/rest/FlightStatusList/getFlightStatusList?ServiceKey={}&pageNo={}'.format(host, serviceKey, 1)
-    headers = {
-        'Content-Type': 'application/xml'
-    }
-    res = requests.get(url, headers=headers)
-    print(res.text)
+    flightinternational = soup.select("item")
+
+    for inter in flightinternational:
+    #기준공항
+    airport = inter.select_one('item > airport').text
+    #운항구간
+    city = inter.select_one('item > city').text
+    #출/입국코드
+    internationalIoType = inter.select_one('item > internationalIoType').text
 
 if __name__ == '__main__':
     get_air_d_flight()
